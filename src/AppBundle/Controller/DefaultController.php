79412d2856ee8b50164dd34652fcc223c5662d58
Tomasz Dobrzeniecki <tomaszdobrzeniecki25@gmail.com>
2017-06-17 11:24:33 +0200

    add country and currency choice type fields

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use Rinvex\Country\Loader;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,7 +14,14 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $northAmerica = Loader::where('geo.continent', ['NA' => 'North America']);
+        $europe = Loader::where('geo.continent', ['EU' => 'Europe']);
+
+        $x = array_merge($europe,$northAmerica);
+        foreach ($x as $k => $v) {
+            dump($v['name']['common']);
+        }
+        die;
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
