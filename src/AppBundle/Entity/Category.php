d1cab5cb7adc0400140815c5602f8bc1a6908e39
Tomasz Dobrzeniecki <tomaszdobrzeniecki25@gmail.com>
2017-06-18 21:08:51 +0200

    add datafixtures

---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AppBundle\Entity;
+use AppBundle\Entity\User;
+/**
+ * Category
+ */
+class Category
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @var \AppBundle\Entity\User
+     */
+    private $user;
+
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     *
+     * @return Category
+     */
+    public function setUser(User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\Common\Collections\Collection;
+use AppBundle\Entity\Category;
 
 class User extends BaseUser
 {
@@ -20,6 +22,11 @@ class User extends BaseUser
      * @var string
      */
     protected $currency;
+
+    /**
+     * @var Collection
+     */
+    private $categories;
 
     public function __construct()
     {
@@ -46,5 +53,37 @@ class User extends BaseUser
         $this->currency = $currency;
     }
 
+    /**
+     * Add category
+     *
+     * @param Category $category
+     *
+     * @return User
+     */
+    public function addCategory(Category $category)
+    {
+        $this->categories[] = $category;
 
+        return $this;
+    }
+
+    /**
+     * Remove category
+     *
+     * @param Category $category
+     */
+    public function removeCategory(Category $category)
+    {
+        $this->categories->removeElement($category);
+    }
+
+    /**
+     * Get categories
+     *
+     * @return Collection
+     */
+    public function getCategories()
+    {
+        return $this->categories;
+    }
 }
